b315732d83b7178b507c4ee3b0b1812bd7904483
Rafael Muñoz Cárdenas <rafael@humanitec.com>
2019-01-15 15:30:12 +0100

    Migrate on entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# It is responsability of the deployment orchestration to execute before
-# migrations, create default admin user, populate minimal data, etc.
+python manage.py migrate
 
 gunicorn location_service.wsgi --config location_service/gunicorn_conf.py
