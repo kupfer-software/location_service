042e19eb3d87cd69c60982fd1e9a4fb11b550cf5
Ralf Kostulski <639859+ralfzen@users.noreply.github.com>
2019-04-01 15:26:35 +0200

    Fix warnings in tests

--- location_service/tests/test_urls.py
+++ location_service/tests/test_urls.py
@@ -1,21 +1,29 @@
+import uuid
+
 from django.test import TestCase
 from django.urls import reverse
 
 
 class ProfileTypeOptionsViewsTest(TestCase):
+    def setUp(self) -> None:
+        session = self.client.session
+        session['jwt_organization_uuid'] = str(uuid.uuid4())
+        session.save()
+
     def test_get_docs_swagger_ui(self):
+        # reverse() would be better
         response = self.client.get(reverse('docs-swagger-ui'))
         self.assertEqual(response.status_code, 200)
 
     def test_get_docs_swagger_raw(self):
-        # I tried to use reverse(), but does not work
+        # reverse() would be better
         response = self.client.get('/docs/swagger.json')
         self.assertEqual(response.status_code, 200)
         self.assertIn(
             b'"info": {"title": "Location Service API", "version": "latest"}',
             response.content)
 
-        # I tried to use reverse(), but does not work
+        # reverse() would be better
         response = self.client.get('/docs/swagger.yaml')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'title: Location Service API', response.content)
