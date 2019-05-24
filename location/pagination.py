ce48e931f1e26621f28e704acaea9e1d6b078272
Ralf Kostulski <639859+ralfzen@users.noreply.github.com>
2019-05-24 16:18:17 +0200

    Increase pagination max limit on SiteProfileViewSet

--- location/pagination.py
+++ location/pagination.py
@@ -3,4 +3,4 @@ from rest_framework.pagination import LimitOffsetPagination
 
 class DefaultLimitOffsetPagination(LimitOffsetPagination):
     default_limit = 50
-    max_limit = 500
+    max_limit = 7000
